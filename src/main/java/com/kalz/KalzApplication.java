1ad13ced4d037175fa6efff81ae65481c10e947c
alwen <alwenperara44@gmail.com>
2025-03-03 21:07:42 +0530

    Initial commit

---
+++ src/main/java/com/kalz/KalzApplication.java
@@ -0,0 +1,13 @@
+package com.kalz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalz/KalzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
